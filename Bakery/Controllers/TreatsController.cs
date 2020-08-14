e36bd0df97010ea92f4dd2bbaa393a8437dfe319
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-14 11:08:56 -0700

    add basic using statements and setup for controllers.

---
+++ Bakery/Controllers/TreatsController.cs
@@ -0,0 +1,18 @@
+using Bakery.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Bakery.Controllers
+{
+  public class TreatsController : Controllers
+  {
+    
+  }
+}
